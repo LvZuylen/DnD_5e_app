e9d2540ad78f223533c4a51cafea127d060afcdd
Lucas van Zuylen (laptop) <ln.vanzuylen@student.avans.nl>
2019-06-04 12:16:05 +0200

    add spells method

--- app/src/main/java/com/example/myapplication/SpellListFragment.java
+++ app/src/main/java/com/example/myapplication/SpellListFragment.java
@@ -1,4 +1,7 @@
 package com.example.myapplication;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,6 +17,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -21,10 +25,12 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONObject;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.ArrayList;
 
@@ -38,40 +44,29 @@ public class SpellListFragment extends Fragment implements SpellAdapter.SpellOnI
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
         spellBook = new ArrayList<>();
         recyclerView = Objects.requireNonNull(getView()).findViewById(R.id.spell_list_view);
+
         // Initialize spells
-
-
-        // Create adapter passing in the sample user data
         adapter = new SpellAdapter(spellBook, this);
-        // Attach the adapter to the recyclerview to populate items
         recyclerView.setAdapter(adapter);
-        // Set layout manager to position the items
         layoutManager = new LinearLayoutManager(getActivity());
         recyclerView.setLayoutManager(layoutManager);
-        // That's all!
-        urls = new String[]{
-                "http://www.dnd5eapi.co/api/spells/42/",
-                "http://www.dnd5eapi.co/api/spells/43/",
-                "http://www.dnd5eapi.co/api/spells/44/",
-                "http://www.dnd5eapi.co/api/spells/45/",
-                "http://www.dnd5eapi.co/api/spells/46/",
-                "http://www.dnd5eapi.co/api/spells/47/",
-                "http://www.dnd5eapi.co/api/spells/48/",
-                "http://www.dnd5eapi.co/api/spells/484/",
-                "http://www.dnd5eapi.co/api/spells/434/",
-                "http://www.dnd5eapi.co/api/spells/435/",
-                "http://www.dnd5eapi.co/api/spells/437/",
-                "http://www.dnd5eapi.co/api/spells/439/",
-                "http://www.dnd5eapi.co/api/spells/345/",
-                "http://www.dnd5eapi.co/api/spells/223/",
-                "http://www.dnd5eapi.co/api/spells/123/",
-                "http://www.dnd5eapi.co/api/spells/119",
-        };
+
+        if(!getListFromPreferences()){
+            defaultSpellBook();
+        }
+
         queue = Volley.newRequestQueue(getActivity());
         addToSpellBook(urls);
-        addMoreSpells("http://www.dnd5eapi.co/api/spells/1");
+        Button addSpell = getActivity().findViewById(R.id.addSpellButton);
+        addSpell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addMoreSpells("http://www.dnd5eapi.co/api/spells/1");
+            }
+        });
     }
 
     @Override
@@ -114,7 +109,58 @@ public class SpellListFragment extends Fragment implements SpellAdapter.SpellOnI
     public void addMoreSpells(String newSpell) {
         String[] spells = new String[]{newSpell};
         addToSpellBook(spells);
+        saveListInPreferences();
     }
+
+    public void defaultSpellBook() {
+        urls = new String[]{
+                "http://www.dnd5eapi.co/api/spells/42/",
+                "http://www.dnd5eapi.co/api/spells/43/",
+                "http://www.dnd5eapi.co/api/spells/44/",
+                "http://www.dnd5eapi.co/api/spells/45/",
+                "http://www.dnd5eapi.co/api/spells/46/",
+                "http://www.dnd5eapi.co/api/spells/47/",
+                "http://www.dnd5eapi.co/api/spells/48/",
+                "http://www.dnd5eapi.co/api/spells/484/",
+                "http://www.dnd5eapi.co/api/spells/434/",
+                "http://www.dnd5eapi.co/api/spells/435/",
+                "http://www.dnd5eapi.co/api/spells/437/",
+                "http://www.dnd5eapi.co/api/spells/439/",
+                "http://www.dnd5eapi.co/api/spells/345/",
+                "http://www.dnd5eapi.co/api/spells/223/",
+                "http://www.dnd5eapi.co/api/spells/123/",
+                "http://www.dnd5eapi.co/api/spells/119",
+        };
+    }
+
+
+    public void saveListInPreferences() {
+        String json = new Gson().toJson(urls);
+        SharedPreferences prefs = getActivity().getSharedPreferences(getString(R.string.pref_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(getString(R.string.pref_key), json);
+        editor.apply();
+    }
+//
+//    public void deleteListFromPreferences() {
+//        SharedPreferences prefs = getActivity().getSharedPreferences(getString(R.string.pref_key), Context.MODE_PRIVATE);
+//        SharedPreferences.Editor editor = prefs.edit();
+//        editor.clear();
+//        editor.apply();
+//        urls = null;
+//    }
+
+    public boolean getListFromPreferences() {
+        SharedPreferences prefs = getActivity().getSharedPreferences(getString(R.string.pref_key), Context.MODE_PRIVATE);
+        if (prefs.contains(getString(R.string.pref_key))) {
+            String prefsString = prefs.getString(getString(R.string.pref_key), "");
+
+            urls = new Gson().fromJson(prefsString, String[].class);
+            return true;
+        }
+        return false;
+    }
+
 
     @Override
     public void onSpellClick(String spellDesc) {
@@ -124,9 +170,6 @@ public class SpellListFragment extends Fragment implements SpellAdapter.SpellOnI
 
         action = SpellListFragmentDirections.actionSpellListViewToSpellViewFragment();
         action.setSpelltext(spellDesc);        // should be:
-        // action = SpellListFragmentDirections.actionSpellListViewToSpellViewFragment(spellDesc);
-        // but it won't take arguments?..
-
         NavHostFragment.findNavController(SpellListFragment.this).navigate(action);
     }
 }
